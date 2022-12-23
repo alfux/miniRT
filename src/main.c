4f81449f53d5e809d43f460756106f53142b620d
alfux <alexis.t.fuchs@gmail.com>
2022-12-23 16:48:11 +0100

    new file:   scene.rt 	modified:   src/ft_exit.c 	modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -6,141 +6,26 @@
 /*   By: efunes <efunes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 00:12:34 by alfux             #+#    #+#             */
-/*   Updated: 2022/12/23 15:36:58 by afuchs           ###   ########.fr       */
+/*   Updated: 2022/12/23 16:45:22 by alfux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <miniRT.h>
-#define DIAM 0.4//0.02f
-#define COORD 0.0f
 #define RESOLUTION 1280
 
 int	main(int argc, char **argv)
 {
 	t_win	window;
-	t_cam	cam;
-	void	*buf;
-	void	*tmp;
 
 	(void)argc;
-	(void)argv;
 	window = ft_setwin(RESOLUTION, (RESOLUTION * 9) / 16, "miniRT");
-	//if (ft_pars(&windows, argv[1])//
-		// return (1); // destroy_img // destroy_windows // free memory struct win
+	if (ft_pars(&window, argv[1]))//
+		return (1); // destroy_img // destroy_windows // free memory struct win
 		// OU
 			// ft_bzero(&win, sizeof(t_win));
 			// ft_pars
 			// ft_setwin // sans bzero
-	cam = ft_setcam(ft_setvec(4, -0.25, 1.2), ft_setvec(-1, 0.15, -0.2), 60);
-	window.scn.cam = &cam;
-	window.scn.amb = ft_setamb(0.1f, ft_setrgb(255, 255, 255));
-	buf = ft_newlig(ft_setvec(0.f, 0.f, .4f), 1.f, ft_setrgb(255, 255, 255));
-	if (!buf)
-		ft_exit_failure(&window, "error: light");
-	window.scn.lig = ft_objnew('L', buf);
-	if (!window.scn.lig && !ft_free(buf))
-		ft_exit_failure(&window, "error: object list");
-//------------------SPHERES-----------------------------------------------------
-	buf = ft_newsph(DIAM, ft_setvec(1.f, 1.f, 1.f), ft_setrgb(255, 255, 255));
-	if (!buf)
-		ft_exit_failure(&window, "error: sphere");
-	tmp = ft_objnew('S', buf);
-	if (!tmp && !ft_free(buf))
-		ft_exit_failure(&window, "error: object list");
-	ft_objadd(&window.scn.obj, tmp);
-	buf = ft_newsph(DIAM, ft_setvec(0.2f, 0.4f, 1.35f), ft_setrgb(0, 0, 255));
-	if (!buf)
-		ft_exit_failure(&window, "error: sphere");
-	tmp = ft_objnew('S', buf);
-	if (!tmp && !ft_free(buf))
-		ft_exit_failure(&window, "error: object list");
-	ft_objadd(&window.scn.obj, tmp);
-	buf = ft_newsph(DIAM, ft_setvec(0.f, 0.f, 2.f), ft_setrgb(0, 0, 255));
-	if (!buf)
-		ft_exit_failure(&window, "error: sphere");
-	tmp = ft_objnew('S', buf);
-	if (!tmp && !ft_free(buf))
-		ft_exit_failure(&window, "error: object list");
-	ft_objadd(&window.scn.obj, tmp);
-	buf = ft_newsph(DIAM, ft_setvec(1.f, 0.f, 0.f), ft_setrgb(255, 0, 0));
-	if (!buf)
-		ft_exit_failure(&window, "error: sphere");
-	tmp = ft_objnew('S', buf);
-	if (!tmp && !ft_free(buf))
-		ft_exit_failure(&window, "error: object list");
-	ft_objadd(&window.scn.obj, tmp);
-	buf = ft_newsph(DIAM, ft_setvec(2.f, 0.f, 0.f), ft_setrgb(255, 0, 0));
-	if (!buf)
-		ft_exit_failure(&window, "error: sphere");
-	tmp = ft_objnew('S', buf);
-	if (!tmp && !ft_free(buf))
-		ft_exit_failure(&window, "error: object list");
-	ft_objadd(&window.scn.obj, tmp);
-	buf = ft_newsph(DIAM, ft_setvec(0.f, 1.f, 0.f), ft_setrgb(0, 255, 0));
-	if (!buf)
-		ft_exit_failure(&window, "error: sphere");
-	tmp = ft_objnew('S', buf);
-	if (!tmp && !ft_free(buf))
-		ft_exit_failure(&window, "error: object list");
-	ft_objadd(&window.scn.obj, tmp);
-	buf = ft_newsph(DIAM, ft_setvec(0.f, 1.5f, -0.5f), ft_setrgb(0, 255, 0));
-	if (!buf)
-		ft_exit_failure(&window, "error: sphere");
-	tmp = ft_objnew('S', buf);
-	if (!tmp && !ft_free(buf))
-		ft_exit_failure(&window, "error: object list");
-	ft_objadd(&window.scn.obj, tmp);
-//------------------PLANES------------------------------------------------------
-	buf = ft_newpla(ft_setvec(0, -0.5f, 0.f), ft_setvec(0.f, 1.f, 0.f),
-			ft_setrgb(255, 0, 255));
-	if (!buf)
-		ft_exit_failure(&window, "error: plane");
-	tmp = ft_objnew('P', buf);
-	if (!tmp && !ft_free(buf))
-		ft_exit_failure(&window, "error: object list");
-	ft_objadd(&window.scn.obj, tmp);
-	buf = ft_newpla(ft_setvec(0, 0.f, -0.5f), ft_setvec(0.f, 0.f, 1.f),
-			ft_setrgb(255, 255, 0));
-	if (!buf)
-		ft_exit_failure(&window, "error: plane");
-	tmp = ft_objnew('P', buf);
-	if (!tmp && !ft_free(buf))
-		ft_exit_failure(&window, "error: object list");
-	ft_objadd(&window.scn.obj, tmp);
-	buf = ft_newpla(ft_setvec(-0.5f, 0.f, 0.f), ft_setvec(1.f, 0.f, 0.f),
-			ft_setrgb(0, 255, 255));
-	if (!buf)
-		ft_exit_failure(&window, "error: plane");
-	tmp = ft_objnew('P', buf);
-	if (!tmp && !ft_free(buf))
-		ft_exit_failure(&window, "error: object list");
-	ft_objadd(&window.scn.obj, tmp);
-//------------------CYLINDERS---------------------------------------------------
-	buf = ft_newcyl(ft_setvec(0.f, 0.f, 1.f), ft_setvec(0.f, 0.f, 1.f),
-			ft_setvec(0.5f, 1.f, 0), ft_setrgb(0, 0, 255));
-	if (!buf)
-		ft_exit_failure(&window, "error: cylinder");
-	tmp = ft_objnew('C', buf);
-	if (!tmp && !ft_free(buf))
-		ft_exit_failure(&window, "error: object list");
-	ft_objadd(&window.scn.obj, tmp);
-	buf = ft_newcyl(ft_setvec(1.0f, 0.f, 0.f), ft_setvec(1.f, 0.f, 0.f),
-			ft_setvec(0.5f, 1.f, 0), ft_setrgb(255, 0, 0));
-	if (!buf)
-		ft_exit_failure(&window, "error: cylinder");
-	tmp = ft_objnew('C', buf);
-	if (!tmp && !ft_free(buf))
-		ft_exit_failure(&window, "error: object list");
-	ft_objadd(&window.scn.obj, tmp);
-	buf = ft_newcyl(ft_setvec(0.f, 0.f, 0.f), ft_setvec(0.f, 1.f, 0.f),
-			ft_setvec(0.5f, 1.f, 0), ft_setrgb(0, 255, 0));
-	if (!buf)
-		ft_exit_failure(&window, "error: cylinder");
-	tmp = ft_objnew('C', buf);
-	if (!tmp && !ft_free(buf))
-		ft_exit_failure(&window, "error: object list");
-	ft_objadd(&window.scn.obj, tmp);
-//------------------------------------------------------------------------------
+	*window.scn.cam = ft_setcam(window.scn.cam->pov, window.scn.cam->dir, window.scn.cam->fov);
 	(void)mlx_hook(window.wid, 17, 0L, &ft_exit_success, &window);
 	(void)mlx_hook(window.wid, 2, 0L, &ft_keyhook, &window);
 	ft_print_scene(window.scn);
