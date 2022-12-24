e17df796b187e52de2b93c797175e428621956e7
alfux <alexis.t.fuchs@gmail.com>
2022-12-24 15:39:04 +0100

    bugfixed:   artefact caused by too-close-to-zero determinant 	adjusted:   any point in a light deadzone is now considered an obstacle
    	modified:   scene.rt
    	modified:   src/ft_raytra.c
    	modified:   src/ft_shades.c
    	modified:   src/ft_syspla.c

--- src/ft_shades.c
+++ src/ft_shades.c
@@ -6,43 +6,42 @@
 /*   By: alfux <alexis.t.fuchs@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 09:22:41 by alfux             #+#    #+#             */
-/*   Updated: 2022/12/23 14:10:57 by alfux            ###   ########.fr       */
+/*   Updated: 2022/12/24 15:22:52 by alfux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <miniRT.h>
 
-static int	ft_is_obstacle(t_2x3 const *i, t_vec const *vec, t_vec const *ndir,
+static int	ft_is_obstacle(t_2x3 const *i, t_vec const *vec, t_vec const *ldir,
 				float norm)
 {
-	if (ft_is_sol(i) && ((ft_scalar(ft_dif_uv(i->top, *vec), *ndir) > 0
-				&& ft_distce(i->top, *vec) < norm)
-			|| (ft_scalar(ft_dif_uv(i->bot, *vec), *ndir) > 0
-				&& ft_distce(i->bot, *vec) < norm)))
-		return (1);
+	if (ft_is_sol(i))
+		if (((ft_scalar(ft_dif_uv(i->top, *vec), *ldir) > 0
+					&& ft_distce(i->top, *vec) < norm)
+				|| (ft_scalar(ft_dif_uv(i->bot, *vec), *ldir) > 0
+					&& ft_distce(i->bot, *vec) < norm)))
+			return (1);
 	return (0);
 }
 
-static int	ft_shadow(t_win const *win, t_vec const *vec, t_vec const *lpos,
-	t_obj const *obj)
+static int	ft_shadow(t_win const *win, t_vec const *vec, t_vec const *lpos)
 {
 	t_obj	*lst;
-	t_vec	ndir;
+	t_vec	ldir;
 	float	norm;
 	t_2x3	obs;
 
 	lst = win->scn.obj;
-	ndir = ft_nrmlze(ft_dif_uv(*lpos, *vec));
+	ldir = ft_nrmlze(ft_dif_uv(*lpos, *vec));
 	norm = ft_distce(*lpos, *vec);
 	while (lst)
 	{
-		obs = ft_sysres(&ndir, vec, lst);
-		if ((ft_deadzn(&obs, vec, EPSILON) && lst == obj )
-			|| ft_deadzn(&obs, lpos, EPSILON))
+		obs = ft_sysres(&ldir, vec, lst);
+		if (ft_deadzn(&obs, vec, EPSILON))
 		{
 			lst = lst->next;
 			continue ;
 		}
-		if (ft_is_obstacle(&obs, vec, &ndir, norm))
+		if (ft_deadzn(&obs, lpos, EPSILON) || ft_is_obstacle(&obs, vec, &ldir, norm))
 			return (1);
 		lst = lst->next;
 	}
@@ -64,7 +63,7 @@ t_rgb	ft_shades(t_win const *win, t_obj const *obj, t_vec const *vec,
 		i = ft_shdcyl(win, (t_cyl *)obj->obj, vec);
 	else
 		i = 0;
-	if (i < 0.f || ft_shadow(win, vec, &((t_lig *)win->scn.lig->obj)->pos, obj))
+	if (i < 0.f || ft_shadow(win, vec, &((t_lig *)win->scn.lig->obj)->pos))
 		i = 0;
 	i = ((t_lig *)win->scn.lig->obj)->rat * i + win->scn.amb.rat * (1 - i);
 	return (ft_setrgb(rgb->r * i, rgb->g * i, rgb->b * i));
