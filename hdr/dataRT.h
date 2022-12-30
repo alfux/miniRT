4c940b3f8b2fac98687cd49dc07f9025c823262d
alfux <alexis.t.fuchs@gmail.com>
2022-12-30 14:25:51 +0100

    modified:   hdr/dataRT.h 	modified:   src/ft_getimp.c

--- hdr/dataRT.h
+++ hdr/dataRT.h
@@ -6,7 +6,7 @@
 /*   By: efunes <efunes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 00:15:04 by alfux             #+#    #+#             */
-/*   Updated: 2022/12/30 13:21:57 by alfux            ###   ########.fr       */
+/*   Updated: 2022/12/30 14:17:42 by alfux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -194,9 +194,9 @@ typedef struct s_win	t_win;
 //Index data structure
 struct					s_idx
 {
-	uint32_t	a;
-	uint32_t	b;
-	uint32_t	c;
+	size_t	a;
+	size_t	b;
+	size_t	c;
 };
 typedef struct s_idx	t_idx;
 
--- src/ft_getimp.c
+++ src/ft_getimp.c
@@ -6,7 +6,7 @@
 /*   By: alfux <alexis.t.fuchs@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 13:11:47 by alfux             #+#    #+#             */
-/*   Updated: 2022/12/30 13:18:09 by alfux            ###   ########.fr       */
+/*   Updated: 2022/12/30 14:25:01 by alfux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,25 @@
 
 t_imp	*ft_getimp(char const *file)
 {
-	int	fd;
+	char const	*line;
+	char const	*first;
+	int			fd;
+	t_idx		n;
 
 	fd = open(file, O_RDONLY);
 	if (fd == -1)
 		return ((t_imp *)0);
+	line = get_next_line(fd);
+	while (line)
+	{
+		first = ft_first(line);
+		if (*first == 'v')
+		{
+			if (ft_isspace(*(first + 1)))
+				n.a++;
+			else if (*(first + 1) == 'n')
+				n.b++;
+				n.c++;
+		}
+	}
 }
