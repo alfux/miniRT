b45ecbeaae9f3cf3d0f1d48e99210dec849a94c7
elise <elise.funes@gmail.com>
2023-01-11 17:33:49 +0100

    protect dir

--- src/ft_newhbol.c
+++ src/ft_newhbol.c
@@ -6,11 +6,22 @@
 /*   By: efunes <efunes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 16:50:43 by efunes            #+#    #+#             */
-/*   Updated: 2023/01/11 16:53:59 by efunes           ###   ########.fr       */
+/*   Updated: 2023/01/11 17:31:57 by efunes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <miniRT.h>
+
+static int	ft_pars_hbol2(t_obj *new, char *str)
+{
+	if (ft_coord(&((t_hbol *)(new->obj))->rat, &str))
+		return (16);
+	if (ft_pars_double(&((t_hbol *)(new->obj))->dia, &str))
+		return (12);
+	if (ft_rgb(&((t_hbol *)(new->obj))->col, &str))
+		return (5);
+	return (0);
+}
 
 int	ft_pars_hbol(t_obj **obj, char *str)
 {
@@ -26,12 +37,13 @@ int	ft_pars_hbol(t_obj **obj, char *str)
 	ft_objadd(obj, new);
 	if (ft_coord(&((t_hbol *)(new->obj))->pos, &str))
 		return (4);
-	if (ft_coord(&((t_hbol *)(new->obj))->dir, &str))
+	if (ft_coord(&((t_hbol *)(new->obj))->dir, &str)
+		|| ((t_hbol *)(new->obj))->dir.x < -1
+		|| ((t_hbol *)(new->obj))->dir.x > 1
+		|| ((t_hbol *)(new->obj))->dir.y < -1
+		|| ((t_hbol *)(new->obj))->dir.y > 1
+		|| ((t_hbol *)(new->obj))->dir.z < -1
+		|| ((t_hbol *)(new->obj))->dir.z > 1)
 		return (4);
-	if (ft_coord(&((t_hbol *)(new->obj))->rat, &str))
-		return (16);
-	if (ft_pars_double(&((t_hbol *)(new->obj))->dia, &str))
-		return (12);
-	if (ft_rgb(&((t_hbol *)(new->obj))->col, &str))
-		return (5);
+	return (ft_pars_hbol2(new, str));
 }
--- src/ft_newpbol.c
+++ src/ft_newpbol.c
@@ -6,11 +6,22 @@
 /*   By: efunes <efunes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 16:06:10 by efunes            #+#    #+#             */
-/*   Updated: 2023/01/11 16:52:28 by efunes           ###   ########.fr       */
+/*   Updated: 2023/01/11 17:33:20 by efunes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <miniRT.h>
+
+static int	ft_pars_hbol2(t_obj *new, char *str)
+{
+	if (ft_coord(&((t_pbol *)(new->obj))->rat, &str))
+		return (16);
+	if (ft_pars_double(&((t_pbol *)(new->obj))->dia, &str))
+		return (12);
+	if (ft_rgb(&((t_pbol *)(new->obj))->col, &str))
+		return (5);
+	return (0);
+}
 
 int	ft_pars_pbol(t_obj **obj, char *str)
 {
@@ -26,12 +37,13 @@ int	ft_pars_pbol(t_obj **obj, char *str)
 	ft_objadd(obj, new);
 	if (ft_coord(&((t_pbol *)(new->obj))->pos, &str))
 		return (4);
-	if (ft_coord(&((t_pbol *)(new->obj))->dir, &str))
+	if (ft_coord(&((t_pbol *)(new->obj))->dir, &str)
+		|| ((t_pbol *)(new->obj))->dir.x < -1
+		|| ((t_pbol *)(new->obj))->dir.x > 1
+		|| ((t_pbol *)(new->obj))->dir.y < -1
+		|| ((t_pbol *)(new->obj))->dir.y > 1
+		|| ((t_pbol *)(new->obj))->dir.z < -1
+		|| ((t_pbol *)(new->obj))->dir.z > 1)
 		return (4);
-	if (ft_coord(&((t_pbol *)(new->obj))->rat, &str))
-		return (16);
-	if (ft_pars_double(&((t_pbol *)(new->obj))->dia, &str))
-		return (12);
-	if (ft_rgb(&((t_pbol *)(new->obj))->col, &str))
-		return (5);
+	return (ft_pars_hbol2(new, str));
 }
