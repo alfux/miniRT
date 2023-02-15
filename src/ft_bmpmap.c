07ee510f9fbb5d35f6f5399e4493a7263bf22c9e
alfux <alexis.t.fuchs@gmail.com>
2023-02-15 17:53:04 +0100

    We can now use XPM files as bumpmap
    	new file:   bmpmap/maface.xpm
    	modified:   maps/cylinder.rt
    	modified:   maps/plane.rt
    	modified:   src/ft_bmpmap.c
    	modified:   src/main.c

--- src/ft_bmpmap.c
+++ src/ft_bmpmap.c
@@ -6,11 +6,12 @@
 /*   By: alfux <alexis.t.fuchs@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 01:36:01 by alfux             #+#    #+#             */
-/*   Updated: 2023/02/14 19:57:25 by alfux            ###   ########.fr       */
+/*   Updated: 2023/02/14 21:24:59 by alfux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <miniRT.h>
+#define RAT 100
 
 t_vec	ft_bmpmap(t_img const *img, double x, double y)
 {
@@ -18,10 +19,10 @@ t_vec	ft_bmpmap(t_img const *img, double x, double y)
 	t_vec	dy;
 	double	a;
 
-	a = 0.1;
+	a = 1;
 	dx = ft_setvec(1, 0,
-			(ft_bmpimg(img, x + a, y) - ft_bmpimg(img, x - a, y)) / (2 * a));
+			(ft_bmpimg(img, RAT * x + a, RAT * y) - ft_bmpimg(img, RAT * x - a, RAT * y)) / (2 * a));
 	dy = ft_setvec(0, 1,
-			(ft_bmpimg(img, x, y + a) - ft_bmpimg(img, x, y - a)) / (2 * a));
+			(ft_bmpimg(img, RAT * x, RAT * y + a) - ft_bmpimg(img, RAT * x, RAT * y - a)) / (2 * a));
 	return (ft_nrmlze(ft_provec(dx, dy)));
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: efunes <efunes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 00:12:34 by alfux             #+#    #+#             */
-/*   Updated: 2023/02/14 19:42:40 by alfux            ###   ########.fr       */
+/*   Updated: 2023/02/15 17:50:51 by alfux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	main(int argc, char **argv)
 	pla->spc.har = 1;
 	pla->bas = ft_set3x3(ft_setvec(0, 0, 1), ft_setvec(1, 0, 0), ft_setvec(0, 1, 0));
 	pla->co2 = ft_setrgb(255, 255, 255);
-	pla->bmp.iid = mlx_xpm_file_to_image(window.cid, "bmpmap/cuir.xpm", &pla->bmp.w, &pla->bmp.h);
+	pla->bmp.iid = mlx_xpm_file_to_image(window.cid, "bmpmap/maface.xpm", &pla->bmp.w, &pla->bmp.h);
 	pla->bmp.iad = mlx_get_data_addr(pla->bmp.iid, &pla->bmp.bpp, &pla->bmp.opl, &pla->bmp.end);
 //---------------TMP-FOR-TESTS-------------------------------------------------
 //	imp = ft_getimp("models/tetrahedron.obj");
